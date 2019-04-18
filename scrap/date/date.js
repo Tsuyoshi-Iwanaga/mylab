f7716c95cc830bb8258cf4eab7a68a9bdd320caf
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2019-04-18 22:34:49 +0900

    CalTimeSelector plugin

--- scrap/date/date.js
+++ scrap/date/date.js
@@ -1,84 +1,45 @@
-;(function ($){
+;(function($){
   'use strict';
 
-  var Calselector = window.Calselector || {};
+  var CalTimePicker = window.CalTimePicker || {};
 
-  Calselector = (function() {
+  //コンストラクタ
+  CalTimePicker = function(element, options){
+    var _ = this;
 
-    var _Calselector = function(elem, settings) {
-      var _ = this, dataSettings;
-
-      _.defaults = {
-        month: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
-        day: ["日", "月", "火", "水", "木", "金", "土"]
-      }
-
-      _.initials = {
-        animating: false,
-      }
-
-      $.extend(_, _.initials);
-
-      _.init();
-
+    _.initials = {
+      initial_font: 12,
+      max_font: 24,
+      enlarge_ratio: 1.2
     };
 
-    return _Calselector;
-  })();
+    _.settings = $.extend({}, _.initials, options);
 
-  Calselector.prototype.init = function() {
+    _.init(element);
+  };
+
+  //初回実行
+  CalTimePicker.prototype.init = function(element){
+    var _ = this;
+    console.log(_);
+  };
+
+  //メソッド例
+  CalTimePicker.prototype.xxx = function(element) {
     var _ = this;
   };
 
-  Calselector.prototype.bindEvents = function () {
-    this.on('click', function() {
-      $(this)
-    });
-  }
-
-  Calselector.prototype.init = function () {
-  }
-
-  Calselector.prototype.bindEvents = function () {
-  }
-
-  Calselector.prototype.bindEvents = function () {
-  }
-
-  Calselector.prototype.bindEvents = function () {
-  }
-
-  Calselector.prototype.bindEvents = function () {
-  }
-
-  Calselector.prototype.bindEvents = function () {
-  }
-
-  Calselector.prototype.bindEvents = function () {
-  }
-
-  Calselector.prototype.bindEvents = function () {
-  }
-
-  Calselector.prototype.bindEvents = function () {
-  }
-
-  Calselector.prototype.bindEvents = function () {
-  }
-
-  $.fn.slick = function() {
+  //jQueryへ追加
+  $.fn.calTimePicker = function(){
     var _ = this,
-      opt = arguments[0],
-      args = Array.prototype.slice.call(arguments, 1),
-      l = _.length,
-      i,
-      ret;
-    for (i = 0; i < l; i++) {
-      if (typeof opt == 'object' || typeof opt == 'undefined')
-        _[i].calselector = new Calselector(_[i], opt);
-      else
-        ret = _[i].calselector[opt].apply(_[i].calselector, args);
-      if (typeof ret != 'undefined') return ret;
+        opt = arguments[0],
+        l = _.length,
+        i;
+
+    for(i=0; i<l; i++){
+      if(typeof opt == 'object' || typeof opt == 'undefined'){
+        _[i].CalTimePicker = new CalTimePicker(_[i], opt);
+      }
     }
     return _;
   };
