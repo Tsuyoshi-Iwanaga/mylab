01293af6de09537e53fc8479b8e5e6ee26d524b3
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2019-04-21 00:45:33 +0900

    siglton Sample

--- scrap/db_connect190408.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-  $db = new PDO('mysql:dbname=mydb;host=127.0.0.1;charset=utf-8'. 'root', '');
-} catch (PDOException $e) {
-  echo('DB接続エラー'. $e->getMessage());
-}
-
-$count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="peach", price=210');
-echo $count. '件のデータを挿入しました。'
