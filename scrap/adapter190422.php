778a0c8779a30b3a6d8a532544b0e4b9d03a56cf
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2019-04-22 11:11:02 +0900

    20190422

---
+++ scrap/adapter190422.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once '<DisplaySourceFile.class.php';
+require_once 'ShowFile.class.php';
+
+class ShowFile {
+  private $filename;
+
+  public function __construct($filename) {
+    if(!is_readable($filename)) {
+      throw new Exception('file' . $filename . 'is not required!');
+    }
+    $this->filename = $filename;
+  }
+}
+
+public function showPlain() {
+  echo '<pre>',
+  echo htmlspecialchars(file_get_contents($this->filename), ENT_QUOTES, mb_internal_encoding());
+  echo '</pre>';
+}
+
+public function showHighlight() {
+  highlight_file($this->filename);
+}
+}
+
+class DisplaySourceFileImple extends ShowFile implements DisplaySourceFile {
+  public function __construct($filename) {
+    parent::__contruct($filename);
+  }
+  public function display() {
+    parent::showHighlight();
+  }
+}
+
+class DisplaySourceFileImple implements DiplaySourceFile {
+  private $show_file,
+
+  public function __construct($filename) {
+    $this->show_file = new ShowFile($filename);
+  }
+
+  public function display() {
+    $this->show_file->showHighlight();
+  }
+}
+
+require_once 'DisplaySourceFileImpl.class.php';
+
+$show_file = new DisplaySourceFileImpl('./ShowFile.class.php');
+$show_file->display();
