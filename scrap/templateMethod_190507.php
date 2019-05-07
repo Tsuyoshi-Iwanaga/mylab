855f97ee840b8e65e345301871bbbaac3348eebf
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2019-05-07 11:39:53 +0900

    add templete Method 190507

---
+++ scrap/templateMethod_190507.php
@@ -0,0 +1,71 @@
+<?php
+
+abstract class AbstractDisplay {
+  private $data;
+
+  public function __construct($data) {
+    if(!is_array($data)) {
+      $data = array($data);
+    }
+    $this->data = $data;
+  }
+
+  public function display() {
+    $this->displayHeader();
+    $this->displayBody();
+    $this->displayFooter();
+  }
+
+  public function getData() {
+    return $this->data;
+  }
+
+  protected abstract function displayHeader();
+  protected abstract function displayBody();
+  protected abstract function displayFooter();
+}
+
+class ListDisplay extends AbstractDisplay {
+  protected function displayHeader() {
+    echo '<dl>';
+  }
+
+  protected function displayBody() {
+    foreach($this->getData() as $key => $value) {
+      echo '<dt>Item'. $key . '</dt>';
+      echo '<dd>'. $value . '</dd>';
+    }
+  }
+
+  protected function displayFooter() {
+    echo '</dl>';
+  }
+}
+
+class TableDisplay extends AbstractDisplay {
+  protected function displayHeader() {
+    echo '<table>';
+  }
+
+  protected function displayBody() {
+    foreach($this->getData() as $key => $value) {
+      echo '<tr>';
+      echo '<th>'. $key . '</th>';
+      echo '<td>'. $value . '</td>';
+      echo '</tr>';
+    }
+  }
+
+  protected function displayFooter() {
+    echo '</table>';
+  }
+}
+
+//clientCode
+$data = ['Design Pattern', 'Gang of four', 'Template Method'];
+
+$display1 = new ListDisplay($data);
+$display2 = new TableDisplay($data);
+
+$display1->display();
+$display2->display();
