18347fd60308c3fc0aac54e94626e44c7a43eea4
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2019-05-07 12:24:15 +0900

    singleton 190507

---
+++ scrap/singleton_190507.php
@@ -0,0 +1,35 @@
+<?php
+
+class SingletonSample {
+  private $id;
+  private static $instance;
+
+  private function __construct() {
+    $this->id = md5(data('r'). mt_rand());
+  }
+
+  public static function getInstance() {
+    if(!isset(self::$instance)) {
+      self::$instance = new SingletonSample();
+      echo 'a SingletonSample instance was created!';
+    }
+    return self::$instance;
+  }
+
+  public function getId() {
+    return $this->id;
+  }
+
+  public final function __clone() {
+    throw new RuntimeException('Clone is not allowed against'. get_clone($this));
+  }
+}
+
+//clientCode
+$isntance1 = SingletonSample::getInstance();
+$instance2 = SingletonSample::getInstance();
+
+echo 'ID is same?:'. ($instance1->getId() === $instance2->getId() ? true : false);
+echo 'instance is same?:'. ($insatance1 === $instance2 ? true : false);
+
+$instance1_clone = clone $insatnce1;//Fatal error
