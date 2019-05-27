267b84f1dee6a88014ead189b9683be62a720e91
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2019-05-27 19:30:22 +0900

    bridge

--- scrap/php_dp/bridge.php
+++ scrap/php_dp/bridge.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set('display_errors', "On");
+
+//----------どう実装するのか----------//
 interface DataSource {
   public function open();
   public function read();
@@ -7,24 +10,24 @@ interface DataSource {
 }
 
 class FileDataSource implements DataSource {
-  private $source_name;
+  private $file_name;
   private $handler;
 
-  function __construct($source_name) {
-    $this->source_name = $source_name;
+  public function __construct($file_name) {
+    $this->file_name = $file_name;
   }
 
-  function open() {
-    if(!is_readable($this->source_name)) {
+  public function open() {
+    if(!is_readable($this->file_name)) {
       throw new Exception('データソースがみつかりません');
     }
-    $this->handler = fopen($this->source_name, 'r');
+    $this->handler = fopen($this->file_name, 'r');
     if(!$this->handler) {
-      throw new Exception('データソースのオープンに失敗しました');
+      throw new Exception('データソースのオープンに失敗しました。');
     }
   }
 
-  function read() {
+  public function read() {
     $buffer = array();
     while(!feof($this->handler)) {
       $buffer[] = fgets($this->handler);
@@ -32,42 +35,48 @@ class FileDataSource implements DataSource {
     return join($buffer);
   }
 
-  function close() {
+  public function close() {
     if(!is_null($this->handler)) {
       fclose($this->handler);
     }
   }
 }
 
+//----------何をするのか----------//
+
 class Listing {
   private $data_source;
 
-  function __construct($data_source) {
+  public function __construct($data_source) {
+    $this->data_source = $data_source;
+  }
+
+  public function open() {
     $this->data_source->open();
   }
 
-  function read() {
+  public function read() {
     return $this->data_source->read();
   }
 
-  function close() {
+  public function close() {
     $this->data_source->close();
   }
 }
 
 class ExtendedListing extends Listing {
-  function __construct($data_source) {
+  public function __construct($data_source) {
     parent::__construct($data_source);
   }
 
-  function readWithEncode() {
-    return htmlspecialchars($this->read(), ENT_QUOTES, mb_convert_encoding());
+  public function readWithEncode() {
+    return htmlspecialchars($this->read(), ENT_QUOTES);
   }
 }
 
 //clientCode
-$list1 = new Listing(new FileDataSource('data.txt'));
-$list2 = new ExtendedListing(new FileDataSource('data.txt'));
+$list1 = new Listing(new FileDataSource('./src/data.txt'));
+$list2 = new ExtendedListing(new FileDataSource('./src/data.txt'));
 
 try {
   $list1->open();
@@ -78,6 +87,8 @@ try {
 
 $data = $list1->read();
 echo $data;
+
+echo '<hr>';
 
 $data = $list2->readWithEncode();
 echo $data;
