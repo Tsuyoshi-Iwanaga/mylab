89bf82c7f351523fa7088e76814a3b6ae296f569
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2019-05-31 20:43:32 +0900

    chain of responsibility

--- scrap/php_dp/chainOfResponsibility.php
+++ scrap/php_dp/chainOfResponsibility.php
@@ -1,5 +1,44 @@
 <?php
-class AlpabetValidationHeader extends ValidationHandler {
+
+ini_set('display_errors', "On");
+
+//Handler
+abstract class ValidationHandler {
+  private $next_handler;
+
+  public function __construct() {
+    $this->next_handler = null;
+  }
+
+  public function setHandler(ValidationHandler $handler) {
+    $this->next_handler = $handler;
+    return $this;
+  }
+
+  public function getNextHandler() {
+    return $this->next_handler;
+  }
+
+  //チェーンの実行
+  public function validate($input) {
+    $result = $this->execValidation($input);
+
+    if(!$result) {
+      return $this->getErrorMessage();
+    } elseif (!is_null($this->getNextHandler())) {
+      return $this->getNextHandler()->validate($input);
+    } else {
+      return true;
+    }
+  }
+
+  protected abstract function execValidation($input);
+
+  protected abstract function getErrorMessage();
+}
+
+//Concrete Handler
+class AlphabetValidationHandler extends ValidationHandler {
   protected function execValidation($input) {
     return preg_match('/^[a-z]*$/i', $input);
   }
@@ -34,8 +73,9 @@ class MaxLengthValidationHandler extends ValidationHandler {
 
   public function __construct($max_length = 10) {
     parent::__construct();
+
     if(preg_match('/^[0-9]{,2}$/', $max_length)) {
-      throw new RuntimeException('max length is invalid (0-99)!');
+      throw new RuntimeException('max length is invalid! (0-99)');
     }
     $this->max_length = (int)$max_length;
   }
@@ -45,12 +85,14 @@ class MaxLengthValidationHandler extends ValidationHandler {
   }
 
   protected function getErrorMessage() {
-    return $this->max_length . 'バイト以内で入力してください';
+    return $this->max_length. 'バイト以内で入力してください';
   }
 }
 
-if(isset($_POST['validation_type']) && isset($_POST['input'])) {
-  $validation_tyle = $_POST['validate_type'];
+
+//clientCode
+if(isset($_POST['validate_type']) && isset($_POST['input'])) {
+  $validate_type = $_POST['validate_type'];
   $input = $_POST['input'];
 
   $not_null_handler = new NotNullValidationHandler();
@@ -59,11 +101,9 @@ if(isset($_POST['validation_type']) && isset($_POST['input'])) {
   $option_handler = null;
   switch($validate_type) {
     case 1:
-      include_once 'AlphabetValidationHandler.class.php';
       $option_handler = new AlphabetValidationHandler();
       break;
     case 2:
-      include_once 'NumberValidationHandler.class.php';
       $option_handler = new NumberValidationHandler();
       break;
   }
@@ -76,7 +116,7 @@ if(isset($_POST['validation_type']) && isset($_POST['input'])) {
   $result = $handler->validate($_POST['input']);
   if($result === false) {
     echo '検証できませんでした';
-  } elseif(is_string($result) && $result !== ''){
+  } elseif(is_string($result) && $result !== '') {
     echo '<p style="color: #dd0000;">'. $result .'</p>';
   } else {
     echo '<p style="color: #008800;">OK</p>';
@@ -85,13 +125,13 @@ if(isset($_POST['validation_type']) && isset($_POST['input'])) {
 ?>
 <form action="" method="post">
   <div>
-    値：<input type="text" name="input">
+    値:<input type="text" name="input">
   </div>
   <div>
-    検証内容：<select name="validate_type">
-    <option value="0">任意</option>
-    <option value="1">半角英字で入力されているか</option>
-    <option value="2">半角数字で入力されているか</option>
+    検証内容:<select name="validate_type">
+      <option value="0">任意</option>
+      <option value="1">半角英字で入力されているか</option>
+      <option value="2">半角数字で入力されているか</option>
     </select>
   </div>
   <div>
