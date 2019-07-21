eb1e72bd59eb666c67837d46738f1b45a9ec426c
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2019-07-21 10:22:20 +0900

    add vue.js sample

---
+++ vue_sample/js/main.js
@@ -0,0 +1,106 @@
+;(function(){
+  'use strict';
+
+  const vm = new Vue({
+    //Target
+    el: '#app',
+
+    //Models
+    data: {
+      todos: [],
+      newItem: ''
+    },
+
+    //監視
+    watch: {
+      todos: {
+        handler: function() {
+          localStorage.setItem('todos', JSON.stringify(this.todos));
+        },
+        deep: true
+      }
+    },
+
+    //ライフサイクル
+    mounted: function() {
+      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+    },
+
+    //Methods
+    methods: {
+      addItem: function() {
+        this.todos.push({
+          title: this.newItem,
+          isDone: false
+        });
+        this.newItem = '';
+      },
+      deleteItem: function(index) {
+        if(confirm('are you sure?')) {
+          this.todos.splice(index, 1);
+        }
+      },
+      purge: function() {
+        if(!confirm('delete finished?')) {
+          return;
+        }
+        this.todos = this.remaining;
+      }
+    },
+
+    //算出プロパティ
+    computed: {
+      remaining: function() {
+        var items = this.todos.filter(todo => {
+          return !todo.isDone;
+        });
+        return items;
+      }
+    },
+  });
+
+  //==== Component ====
+  const likeComponent = Vue.extend({
+    template: '<button @click="countUp">{{message}} {{count}}</button>',
+
+    props: {
+      message: {
+        type: String,
+        default: 'Like'
+      }
+    },
+
+    //コンポーネントのデータは必ず関数である必要がある
+    data: function() {
+      return {
+        count: 0
+      }
+    },
+
+    methods: {
+      countUp: function() {
+        this.count++;
+        this.$emit('increment');
+      }
+    }
+  });
+
+  const btnArea = new Vue({
+    el: '#wrap01',
+
+    components: {
+      'like-component': likeComponent
+    },
+
+    data: {
+      total: 0
+    },
+
+    methods: {
+      incrementTotal: function() {
+        this.total++;
+      }
+    }
+  });
+
+})();
