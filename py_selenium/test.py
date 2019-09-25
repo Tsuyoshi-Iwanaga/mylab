d0a008d4fee34a44052cdfee852ac20187e3906d
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2019-09-25 23:05:39 +0900

    update python selenium

--- py_selenium/test.py
+++ py_selenium/test.py
@@ -3,6 +3,7 @@ from selenium import webdriver #pip install selenium
 import chromedriver_binary #pip install chromedriver-binary==77.0.3865.40.0
 import pandas as pd #pip install pandas
 import sys
+import re
 
 driver = webdriver.Chrome()
 
@@ -10,11 +11,15 @@ driver = webdriver.Chrome()
 asinList = pd.read_csv('./asinList.csv', encoding="UTF-8")
 
 for asin in asinList['ASIN']:
+    #ページを読み込む
     url = 'https://www.amazon.co.jp/dp/' + asin + '/'
-    print(url)
     driver.get(url)
-    for price in driver.find_elements_by_id('availability'):
-      print(price.text)
+
+    #セレクタで取得、要素をテキストで表示
+    for price in driver.find_elements_by_css_selector('#reviewsMedley .AverageCustomerReviews'):
+      groups = re.search(r'(\d\.\d)', price.text)
+      print(groups.group())
+
     time.sleep(5)
 
 driver.quit()
