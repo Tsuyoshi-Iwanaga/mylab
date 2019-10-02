0ec6ea0ec7692e6d547504e797859a534c9773b8
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2019-10-02 11:48:29 +0900

    add Global styles & reset.css

--- ts_cli/src/pages/simulation/simulation.ts
+++ ts_cli/src/pages/simulation/simulation.ts
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import "../../assets/common/scss/global.scss";
 import SimulatorContainer from "../../components/simulators/simulatorContainer.vue";
 
 Vue.config.productionTip = false;
--- ts_cli/src/pages/top/top.ts
+++ ts_cli/src/pages/top/top.ts
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import "../../assets/common/scss/global.scss";
 import Top from "../../components/top/top.vue";
 
 Vue.config.productionTip = false;
