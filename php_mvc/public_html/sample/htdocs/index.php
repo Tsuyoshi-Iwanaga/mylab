2e5567aabca2c380280e21c0fc2c582311aa7405
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2019-12-18 18:49:07 +0900

    arrangement

--- php_mvc/public_html/sample/htdocs/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-ini_set('display_errors', "On");
-
-require_once(dirname(__FILE__).'/../../../library/mvc/Dispatch.php');
-
-$dispatcher = new Dispatcher();
-$dispatcher->setSystemRoot(dirname(__FILE__).'/../');
-$dispatcher->dispatch();
-?>
--- php_mvc/public_html/sample/views/templates/IndexView.tpl
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Index</title>
-</head>
-<body>
-<p>{$hoge}</p>
-</body>
-</html>
