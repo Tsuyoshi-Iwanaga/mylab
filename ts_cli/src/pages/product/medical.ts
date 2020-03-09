20ebfff97ca94629a3c05834f2fbac68c00e5fb3
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2020-03-09 13:35:48 +0900

    source arrangement

--- ts_cli/src/pages/product/medical.ts
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import "../../assets/common/scss/global.scss";
-import Medical from "../../components/product/product_medical.vue";
-
-Vue.config.productionTip = false;
-
-new Vue({
-  render: h => h(Medical)
-}).$mount("#container");
--- ts_cli/src/pages/top/top.ts
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import "../../assets/common/scss/global.scss";
-import Index from "../../components/top/index.vue";
-
-Vue.config.productionTip = false;
-
-new Vue({
-  render: h => h(Index)
-}).$mount("#container");
