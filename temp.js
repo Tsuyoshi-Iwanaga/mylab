fbcc246bf546c80d0b748b53865e6ed34c672101
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2020-03-12 21:07:03 +0900

    async function sample

--- temp.js
+++ temp.js
@@ -1,13 +1,15 @@
-function curry(f){
-  return function _curry(xs){
-      return xs.length < f.length ? function(x){ return _curry(xs.concat([x])); } : f.apply(undefined, xs);
-  }([]);
+async function double(x){
+  if (x % 3 === 0){
+    throw new Error('failed to double');
+  }else{
+    return x*2;
+  }
 }
 
-var _max = curry(Math.max);
+async function main() {
+  const x = 6;
+  const result1 = await double(x).catch(e => 0);
+  console.log(result1);
+}
 
-var brightness = [0.2, -0.3, 1.0, -0.5];
-brightness = brightness.map(curry(Math.max)(0));
-
-console.log(brightness);
-
+main();
