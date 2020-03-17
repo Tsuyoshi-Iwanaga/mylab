880fe7c8a23468446f6c2f06c7010bdf4785dadd
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2020-03-17 22:51:28 +0900

    modify temp.php

--- temp.php
+++ temp.php
@@ -1,2 +1,50 @@
 <?php
 ini_set('display_errors', 'On');
+
+abstract class ReadItemDataStrategy
+{
+  private $filename;
+
+  public function __construct($filename) {
+    $this->filename = $filename;
+  }
+
+  public function getData() {
+    return $this->readData($this->getFilename());
+  }
+
+  public function getFilename() {
+    return $this->filename;
+  }
+
+  abstract protected function readData($filename);
+}
+
+class ReadJsonDataStrategy extends ReadItemDataStrategy
+{
+  protected function readData($filename) {
+    return 'data';
+  }
+}
+
+class ReadTabSeparatedDataStrategy extends ReadItemDataStrategy {
+  protected function readData($filename) {
+    return 'hoge';
+  }
+}
+
+class ItemDateContext
+{
+  private $strategy;
+
+  public function __construct(ReadItemDataStrategy $strategy) {
+    $this->strategy = $strategy;
+  }
+
+  public function getItemData() {
+    return $this->strategy->getData();
+  }
+}
+
+$context = new ItemDateContext(new ReadTabSeparatorDataStrategy('a.txt'));
+$context->getItemData();
