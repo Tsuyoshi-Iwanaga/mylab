a128d44b19a8cc847b66f263ba0ee7861ec1aed6
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2020-03-24 23:28:40 +0900

    chain of responsibility

--- temp.php
+++ temp.php
@@ -1,32 +1,90 @@
 <?php
 ini_set('display_errors', 'On');
 
-class Singleton {
-  private $id;
-  private static $instance;
+abstract class ValidationHandler {
+  private $next_handler;
 
-  private function __construct() {
-    $this->id = md5(date('r').mt_rand());
+  public function __construct() {
+    $this->next_handler = null;
   }
 
-  public static function getInstance() {
-    if(!isset(self::$instance)) {
-      self::$instance = new Singleton();
+  public function setHandler(ValidationHandler $handler) {
+    $this->next_handler = $handler;
+    return $this;
+  }
+
+  public function getNextHandler() {
+    return $this->next_handler;
+  }
+
+  public function validate($input) {
+    $result = $this->execValidation($input);
+
+    if(!$result) {
+      return $this->getErrorMessage();
+    } elseif (!is_null($this->getNextHandler())) {
+      return $this->getNextHandler()->validate($input);
+    } else {
+      return true;
     }
-    return self::$instande;
   }
 
-  public function getId() {
-    return $this->id;
+  abstract protected function execValidation($input);
+
+  abstract protected function getErrorMessage();
+}
+
+class NotNullValidationHandler extends ValidationHander {
+
+  protected function execValidation($input) {
+    return (is_string($input) && $input !== '');
   }
 
-  public final function __clone() {
-    throw new RuntimeException('Clone is not allowed aginst'.get_class($this));
+  protected function getErrorMessage() {
+    return '入力されていません';
   }
 }
 
-$instance01 = Singleton::getInstance();
-$instande02 = Singleton::getInstance();
+class MaxLengthValidationHandler extends ValidationHandler {
+  
+  private $max_length;
 
-$instance01->getId();
-$instance02->getId();
+  public function __construct($max_length = 10) {
+    parent::__construct();
+
+    if(preg_match('/^[0-9]{,2}$/', $max_length)) {
+      throw new RuntimeException('max length is invalid!');
+    }
+    $this->max_length = (int)$max_length;
+  }
+
+  protected function execValidation($input) {
+    return (mb_strlen($input) <= $this->max_length);
+  }
+
+  protected function getErrorMessage() {
+    return $this->max_length.'文字以内で入力してください';
+  }
+}
+
+class AlphabetValidationHandler extends ValidationHander {
+
+  protected function execValidation($input) {
+    return preg_match('/^[a-z]*$/i', $input);
+  }
+
+  protected function getErrorMessage() {
+    return '半角英字で入力してください';
+  }
+}
+
+$input = 'hogehoge';
+
+$handler01 = new NotNullValidationHandler();
+$handler02 = new MaxLengthValidationHandler(8);
+$handler03 = new AlphabetValidationHandler();
+
+$handler01->setHandler($handler02);
+$handler02->setHandler($handler03);
+
+$result = $handler01->validate($input);
