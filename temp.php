c6de139066770c2b0fb8147752cb06d4d32f6bab
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2020-03-26 13:27:27 +0900

    strategy

--- temp.php
+++ temp.php
@@ -1,40 +1,100 @@
 <?php
 ini_set('display_errors', 'On');
 
-class ShowFile {
+abstract class ReadItemDataStrategy
+{
   private $filename;
 
   public function __construct($filename) {
-    if(!is_readable($filename)) {
-      throw new Exception("${filename} is not readable!");
-    }
     $this->filename = $filename;
   }
 
-  public function showPlain() {
-    echo file_get_contents($this->filename);
+  public function getData() {
+    if (!is_readable($this->getFilename())) {
+      throw new \Exception('file is not readable');
+    }
+    return $this->readData($this->getFilename());
   }
 
-  public function showHighlight() {
-    highlight_file($this->filename);
+  public function getFilename() {
+    return $this->filename;
+  }
+
+  abstract protected function readData($filename);
+}
+
+class ReadJsonDataStrategy extends ReadItemDataStrategy
+{
+  protected function readData($filename) {
+    $data = json_decode(file_get_contents($filename));
+
+    foreach ($data as $line) {
+      $obj = new stdClass();
+      $obj->item_name = $line->item_name;
+      $obj->item_id = $line->item_id;
+      $obj->price = $line->price;
+      $obj->release_date = new DateTime($line->release_date);
+
+      $return_value[] = $obj;
+    }
+
+    return $return_value;
   }
 }
 
-interface DisplaySourceFile {
-  public function display();
+class ReadTabSeparetedDataStrategy extends ReadItemDataStrategy {
+
+  protected function readData($filename) {
+    $fp = fopen($filename, 'r');
+    $dummy = fgets($fp, 4096);
+    $return_value = array();
+
+    while(($buffer = fgets($fp, 4096)) !== false) {
+      $data = explode("\t", trim($buffer));
+
+      if(count($data) !== 4) {
+        continue;
+      }
+
+      list($item_id, $item_name, $price, $release_date) = $data;
+
+      $obj = new \stdClass();
+      $obj->item_name = $item_name;
+      $obj->item_id = $item_id;
+      $obj->price = $price;
+      $obj->release_date = new \DateTime($release_date);
+
+      $return_value[] = $obj;
+    }
+
+    fclose($fp);
+
+    return $return_value;
+  }
 }
 
-class DisplaySourceFileImpl implements DisplaySourceFile {
-  private $show_file;
+class ItemDataContext
+{
+  private $strategy;
 
-  public function __construct($filename) {
-    $this->show_file = new ShowFile($filename);
+  public function __construct(ReadItemDataStrategy $strategy) {
+    $this->strategy = $strategy;
   }
 
-  public function display() {
-    $this->show_file->showHightlight();
+  public function getItemData() {
+    return $this->strategy->getData();
   }
 }
 
-$show_file = new DisplaySourceFileImppl(dirname(__FILE__).'/src/test.txt');
-$show_file->display();
+function dumpData($data)
+{
+  foreach ($data as $object) {
+    echo $object->item_name.'<br>';
+  }
+}
+
+$context = new ItemDataContext(new ReadTabSepareatedDataStrategy(__DIR__.'/src/item_data.txt'));
+dumpData($context->getItemData());
+
+$context = new ItemDataContext(new ReadJsonDataStrategy(__DIR__, '/src/item_data.json'));
+dumpData($context->getItemData());
