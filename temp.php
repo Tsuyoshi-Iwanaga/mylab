b5994588ed993c1c1966afb9a9cfbe19219288cd
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2020-03-28 13:46:58 +0900

    strategy

--- temp.php
+++ temp.php
@@ -23,52 +23,18 @@ abstract class ReadItemDataStrategy
   abstract protected function readData($filename);
 }
 
-class ReadJsonDataStrategy extends ReadItemDataStrategy
+class ReadJSONDataStrategy extends ReadItemDataStrategy
 {
   protected function readData($filename) {
     $data = json_decode(file_get_contents($filename));
-
-    foreach ($data as $line) {
-      $obj = new stdClass();
-      $obj->item_name = $line->item_name;
-      $obj->item_id = $line->item_id;
-      $obj->price = $line->price;
-      $obj->release_date = new DateTime($line->release_date);
-
-      $return_value[] = $obj;
-    }
-
     return $return_value;
   }
 }
 
-class ReadTabSeparetedDataStrategy extends ReadItemDataStrategy {
-
+class ReadTabSeparatedDataStrategy extends ReadItemDataStrategy {
   protected function readData($filename) {
     $fp = fopen($filename, 'r');
-    $dummy = fgets($fp, 4096);
-    $return_value = array();
-
-    while(($buffer = fgets($fp, 4096)) !== false) {
-      $data = explode("\t", trim($buffer));
-
-      if(count($data) !== 4) {
-        continue;
-      }
-
-      list($item_id, $item_name, $price, $release_date) = $data;
-
-      $obj = new \stdClass();
-      $obj->item_name = $item_name;
-      $obj->item_id = $item_id;
-      $obj->price = $price;
-      $obj->release_date = new \DateTime($release_date);
-
-      $return_value[] = $obj;
-    }
-
     fclose($fp);
-
     return $return_value;
   }
 }
@@ -86,15 +52,6 @@ class ItemDataContext
   }
 }
 
-function dumpData($data)
-{
-  foreach ($data as $object) {
-    echo $object->item_name.'<br>';
-  }
-}
-
-$context = new ItemDataContext(new ReadTabSepareatedDataStrategy(__DIR__.'/src/item_data.txt'));
-dumpData($context->getItemData());
-
-$context = new ItemDataContext(new ReadJsonDataStrategy(__DIR__, '/src/item_data.json'));
+echo 'TabData';
+$context = new ItemDataContext(new TeadTabSeparatedDataStrategy(__DIR__.'/src/item_data.txt'));
 dumpData($context->getItemData());
