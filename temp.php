aabd6a23ff0b9581c78f0e7f87bec6935ed20ac0
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2020-04-14 18:27:10 +0900

    session

--- temp.php
+++ temp.php
@@ -1,122 +1,57 @@
 <?php
 
-abstract class Application
+class Session
 {
-  protected $debug = false;
-  protected $request;
-  protected $response;
-  protected $session;
-  protected $db_manager;
+  protected static $sessionStarted = false;
+  protected static $sessionIdRegenerated = false;
 
-  public function __construct($debug = false)
+  public function __construct()
   {
-    $this->setDebugMode($debug);
-    $this->initialize();
-    $this->configure();
-  }
-
-  public function setDebugMode($debug) {
-    if($debug) {
-      $this->debug = true;
-      ini_set('display_errors', 1);
-      error_reporting(-1);
-    } else {
-      $this->debug = false;
-      ini_set('display_errors', 0);
+    if(!self::$sessionStarted) {
+      session_start();
+      self::$sessionStarted = true;
     }
   }
 
-  public function initialize()
+  public function set($name, $value)
   {
-    $this->request = new Request();
-    $this->response = new Response();
-    $this->session = new Session();
-    $this->db_manager = new Db_manager();
-    $this->router = new Router($this->registerRoutes());
+    $_SESSION[$name] = $value;
   }
 
-  protected function configure() {}
-
-  abstract public function getRootDir();
-
-  abstract protected function registerRoutes();
-
-  public function isDebugMode()
+  public function get($name)
   {
-    return $this->debug;
-  }
-
-  public function getRequest()
-  {
-    return $this->request;
-  }
-
-  public function getResponse()
-  {
-    return $this->response;
-  }
-
-  public function getSession()
-  {
-    return $this->session;
-  }
-
-  public function getDbManager()
-  {
-    return $this->db_manager;
-  }
-
-  public function getControllerDir()
-  {
-    return $this->getRootDir(). '/controllers';
-  }
-
-  public function getViewDir()
-  {
-    return $this->getRootDir(). '/views';
-  }
-
-  public function getModelDir()
-  {
-    return $this->getRootDir(). '/models';
-  }
-
-  public function getWebDir()
-  {
-    return $this->getRootDir(). '/web';
-  }
-
-  public function run()
-  {
-    $params = $this->router->resolve($this->request->getPathInfo());
-    if($params === false) {
-      //todo-A
+    if(isset($_SESSION[$name])) {
+      return $_SESSION[$name];
     }
-
-    $controller = $params['controller'];
-    $action = $params['action'];
-
-    $this->runAction($controller, $action, $params);
-
-    $this->response->send();
+    return $default;
   }
 
-  public function runAction($controller_name, $action, $params = []) {
-    $controller_class = ucfirst($controller_name). 'Controller';
-
-    $controller = $this->findController($controller_name);
-    if($controller === false) {
-      //todo-B
-    }
-
-    $content = $controller->run($action, $params);
-
-    $this->response->setContent($content);
+  public function remove($name)
+  {
+    unset($_SESSION[$name]);
   }
 
-  protected function findControllers($controller_class) {
-    if(!class_exists($controller_class)) {
-      $controller_file = $this->getControllerDir(). '/'. $controller_class. '.php';
+  public function clear()
+  {
+    $_SESSION = [];
+  }
+
+  public function regenerate($destroy = true)
+  {
+    if(!self::$sessionStarted) {
+      session_regenerate_id($destroy);
+      self::$sessionIdRegenerated = true;
     }
+  }
+
+  public function setAuthenticated($bool)
+  {
+    $this->set('_authenticated', (bool)$bool);
+    $this->regenerate();
+  }
+
+  public function isAuthenticated()
+  {
+    return $this->get('_authenticated', false);
   }
 }
