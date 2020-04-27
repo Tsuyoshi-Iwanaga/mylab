75da53f9976af0dd1bd04d83c694f8c66142893b
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2020-04-27 17:50:05 +0900

    Application

--- temp.php
+++ temp.php
@@ -1,46 +1,155 @@
 <?php
 
-class Router
+abstract class Application
 {
-  protected $routes;
+  protected $debug = false;
+  protected $request;
+  protected $response;
+  protected $session;
+  protected $db_manager;
 
-  public function __construct($definitions)
+  public __construct($debug = false)
   {
-    $this->routes = $this->compileRoutes($definitions);
+    $this->setDebugMode($debug);
+    $this->initialize();
+    $this->configure();
   }
 
-  public function compileRoutes($definitions)
+  protected function setDebugMode($debug)
   {
-    $routes = [];
+    if($debug) {
+      $this->debug = true;
+      ini_set('display_errors', 1);
+      error_reporting(-1);
+    } else {
+      $this->debug = false;
+      ini_set('display_errors', 0);
+    }
+  }
 
-    foreach($definitions as $url => $params) {
-      $tokens = $url=>explode('/', ltrim($url, '/'));
-      foreach($tokens as $i => $token) {
-        if(strpos($token, ':') === 0) {
-          $name = substr($token, 1);
-          $token = '(?P<'. $name. '>[^/]+)';
+  protected function initialize()
+  {
+    $this->request = new Request();
+    $this->response = new Response();
+    $this->session = new Session();
+    $this->db_manager = new DbManager();
+    $this->router = new Router($this->registerRoutes());
+  }
+
+  protected function configure()
+  {
+  }
+
+  abstract public function getRootDir();
+
+  abstract protected function registerRoutes();
+
+  public function isDebugMode()
+  {
+    return $this->debug;
+  }
+
+  public function getRequest()
+  {
+    return $this->request;
+  }
+
+  public function getResponse()
+  {
+    return $this->response;
+  }
+
+  public function getSession()
+  {
+    return $this->session;
+  }
+
+  public function getDbManager()
+  {
+    return $this->db_manager;
+  }
+
+  public function getControllerDir()
+  {
+    return $this->getRootDir(). '/controllers';
+  }
+
+  public function getViewDir()
+  {
+    return $this->getRootDir(). '/views';
+  }
+
+  public function getModelDir()
+  {
+    return $this->getRootDir(). '/models';
+  }
+
+  public function getWebDir()
+  {
+    return $this->getRootDir(). '/web';
+  }
+
+  public function run()
+  {
+    try {
+      $params = $this->router->resolve($this->request->getPathInfo());
+      if($params === false) {
+        throw new HttpNotFoundException('No route found for '. $this->request->getPathInfo());
+      }
+
+      $controller = $params['controller'];
+      $action = $params['action'];
+
+      $this->runAction($controller, $action, $params);
+
+    } catch (HttpNotFoundException $e) {
+      $this->render404Page($e);
+    } catch (UnauthorizedActionException $e) {
+      list($controller, $action) = $this->login_action;
+      $this->runAction($controller, $action);
+    }
+    $this->response->send();
+  }
+
+  public function runAction($controller_name, $action)
+  {
+    $controller_class = ucfirst($controller_name). 'Controller';
+
+    $controller = $this->findController($controller_class);
+    if($controller === false) {
+      throw new HttpNotFoundException($controller_class . 'controller is not found');
+    }
+
+    $content = $controller->run($action, $params);
+    $this->response->setContent($content);
+  }
+
+  protected function findController($controller_class)
+  {
+    if(!class_exists($controller_class))
+    {
+      $controller_file = $this->getControllerDir(). '/'. $controller_class. '.php';
+      if(!is_readable($controller_file)) {
+        return false;
+      } else {
+        require_once $controller_file;
+
+        if(!class_exists($controller_class)) {
+          return false;
         }
-        $tokens[$i] = $token;
       }
-      $pattern = '/'. implode('/', $tokens);
-      $routes[$pattern] = $params;
     }
-    return $routes;
+    return new $controller_class($this);
   }
 
-  public function resolve($path_info)
+  protected function render404Page($e)
   {
-    if('/' !== substr($path_info, 0, 1)) {
-      $path_info = '/'. $path_info;
-    }
+    $this->response->setStatusCode(404, 'NotFound');
+    $message = $this->isDebugMode() ? $e->getMessage() : 'Page not found.';
+    $message = htmlspecialchars($message, ENT_QUOTES, 'UTF-8');
 
-    foreach($this->routes as $pattern => $params) {
-      if(preg_match('#^'. $pattern .'$#', $path_info, $matches)) {
-        $params = array_merge($params, $matches);
-        return $params;
-      }
-    }
-
-    return false;
+    $this->response->setContent(<<<EOF
+    <body>{$message}</body>
+  EOF);
   }
 }
