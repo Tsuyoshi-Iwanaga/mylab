f9f891af497c7f5c4c9bc813934c2f608e334b73
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2020-07-17 23:17:31 +0900

    20200717

--- temp.php
+++ temp.php
@@ -1,46 +1,71 @@
 <?php
+ini_set('display_errors', 'On');
 
-class Router
-{
-  protected $routes;
+abstract class AbstractDisplay {
+  private $data;
 
-  public function __construct($definitions)
-  {
-    $this->routes = $this->compileRoutes($definitions);
+  public function __counstruct($data) {
+    if(!is_array($data)) {
+      $data = array($data);
+    }
+    $this->data = $data;
   }
 
-  public function compileRoutes($definitions)
-  {
-    $routes = [];
-
-    foreach($definitions as $url => $params) {
-      $tokens = $url=>explode('/', ltrim($url, '/'));
-      foreach($tokens as $i => $token) {
-        if(strpos($token, ':') === 0) {
-          $name = substr($token, 1);
-          $token = '(?P<'. $name. '>[^/]+)';
-        }
-        $tokens[$i] = $token;
-      }
-      $pattern = '/'. implode('/', $tokens);
-      $routes[$pattern] = $params;
-    }
-    return $routes;
+  protected function getData() {
+    return $this->data;
   }
 
-  public function resolve($path_info)
-  {
-    if('/' !== substr($path_info, 0, 1)) {
-      $path_info = '/'. $path_info;
-    }
+  protected abstract function displayHeader();
+  protected abstract function displayBody();
+  protected abstract function displayFooter();
 
-    foreach($this->routes as $pattern => $params) {
-      if(preg_match('#^'. $pattern .'$#', $path_info, $matches)) {
-        $params = array_merge($params, $matches);
-        return $params;
-      }
-    }
-
-    return false;
+  public function display() {
+    $this->displayHeader();
+    $this->displayBody();
+    $this->displayFooter();
   }
 }
+
+class ListDisplay extends AbstractDisplay {
+  protected function displayHeader() {
+    echo '</dl>';
+  }
+
+  protected function displayBody() {
+    foreach($this->getData() as $key => $value) {
+      echo '<dt>Item'. $key. '</dt>';
+      echo '<dd>'. $value. '<dd>';
+    }
+  }
+
+  protected function displayFooter() {
+    echo '</dl>';
+  }
+}
+
+class TableDisplay extends AbstractDisplay {
+  protected function displayHeader() {
+    echo '<table>';
+  }
+
+  protected function displayBody() {
+    foreach($this->getData() as $key => $value) {
+      echo '<tr>';
+      echo '<td>Item'. $key. $value. '</td>';
+      echo '<td>'. $value. '</td>';
+      echo '</tr>';
+    }
+  }
+
+  protected function displayFooter() {
+    echo '</table>';
+  }
+}
+
+$data = ['Design Pattern', 'Gang of four', 'Template Method1', 'TemplateMethod2'];
+
+$display1->display();
+echo '<hr>';
+$display2->display();
+
+?>
