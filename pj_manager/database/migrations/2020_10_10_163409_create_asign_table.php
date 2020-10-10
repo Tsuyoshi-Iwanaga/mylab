abb0fc3d4191f50e01731ab2c14694fc34a8334d
tsuyoshi <tsuyo.1991@gmail.com>
2020-10-11 01:43:23 +0900

    create migration asign & client

---
+++ pj_manager/database/migrations/2020_10_10_163409_create_asign_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAsignTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asign', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('asigner');
+            $table->integer('projectId');
+            $table->string('worker');
+            $table->string('planedHours');
+            $table->string('actualHours');
+            $table->string('billableAmount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asign');
+    }
+}
