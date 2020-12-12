727b6a7fe3c9316a7881cd5cda9dd9acbe28b147
tsuyoshi <tsuyo.1991@gmail.com>
2020-12-13 00:36:47 +0900

    update book app

--- ngtest/src/app/app.module.ts
+++ ngtest/src/app/app.module.ts
@@ -1,22 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DetailsComponent } from './details.component'
-import { EditComponent } from './edit.component'
+import { AppRoutingModule } from './app-routing.module'
+import { CoopModule } from './coop/coop.module'
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    DetailsComponent,
-    EditComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    CoopModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
