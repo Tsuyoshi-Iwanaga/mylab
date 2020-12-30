d2c20c6c32dce2758433ee146e91804e1a5265d6
tsuyoshi <tsuyo.1991@gmail.com>
2020-12-30 11:15:29 +0900

    package components in modules

--- ngtest/src/app/app.module.ts
+++ ngtest/src/app/app.module.ts
@@ -7,26 +7,22 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component'
 import { Cmp201219Component } from './cmp201219/cmp201219.component';
-import { Cmp201226Component } from './cmp201226/cmp201226.component';
-import { DetailComponent } from './cmp201226/detail/detail.component';
-import { EditComponent } from './cmp201226/edit/edit.component';
-import { Cmp201227Component } from './cmp201227/cmp201227.component'
+import { Cmp201227Module } from './cmp201227/cmp201227.module'
+import { Cmp201226Module } from './cmp201226/cmp201226.module'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     Cmp201219Component,
-    Cmp201226Component,
-    DetailComponent,
-    EditComponent,
-    Cmp201227Component,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    Cmp201226Module,
+    Cmp201227Module,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ngtest/src/app/cmp201227/cmp201227.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { Cmp201227Component } from './cmp201227.component'
+
+@NgModule({
+  declarations: [
+    Cmp201227Component,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
+})
+export class Cmp201227Module { }
