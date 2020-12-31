afed460c400c8a84ddb6b3034c4ba3cfe13c9362
tsuyoshi <tsuyo.1991@gmail.com>
2020-12-31 23:23:11 +0900

    life cycle methods

--- ngtest/src/app/cmp2012313/cmp2012313.component.ts
+++ ngtest/src/app/cmp2012313/cmp2012313.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cmp2012313.component.scss']
 })
 export class Cmp2012313Component implements OnInit {
+  show = true
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onchange() {
+    this.show = !this.show
+  }
 }
--- ngtest/src/app/cmp2012313/cmp2012313.module.ts
+++ ngtest/src/app/cmp2012313/cmp2012313.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Cmp2012313Component } from './cmp2012313.component';
-
-
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
-  declarations: [Cmp2012313Component],
+  declarations: [Cmp2012313Component, ChildComponent],
   imports: [
     CommonModule
   ]
