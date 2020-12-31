7485e8a0b93d60ea398a7194e3d2aa6d643c0587
tsuyoshi <tsuyo.1991@gmail.com>
2020-12-31 23:35:42 +0900

    add parent component lifecycle

--- ngtest/src/app/cmp2012313/cmp2012313.component.ts
+++ ngtest/src/app/cmp2012313/cmp2012313.component.ts
@@ -1,19 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-cmp2012313',
   templateUrl: './cmp2012313.component.html',
   styleUrls: ['./cmp2012313.component.scss']
 })
-export class Cmp2012313Component implements OnInit {
+export class Cmp2012313Component implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   show = true
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  current = new Date()
 
   onchange() {
     this.show = !this.show
+    this.current = new Date()
+  }
+
+  constructor() {
+    console.log('constructor')
+  }
+
+  ngOnChanges() {
+    console.log('ngOnChanges')
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit')
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck')
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit')
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked')
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit')
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked')
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy')
   }
 }
