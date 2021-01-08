23309e5a118ded01bbe98b4f0b0bebfdbb2a0836
tsuyoshi <tsuyo.1991@gmail.com>
2021-01-09 01:02:09 +0900

    update test

--- ngtest/src/app/cmp201219/cmp201219.component.spec.ts
+++ ngtest/src/app/cmp201219/cmp201219.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
 
 import { Cmp201219Component } from './cmp201219.component';
 
@@ -8,7 +10,14 @@ describe('Cmp201219Component', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ Cmp201219Component ]
+      declarations: [ Cmp201219Component ],
+      imports: [ RouterTestingModule ],
+      providers: [{
+        provide: ActivatedRoute,
+        useValue: {
+          snapshot: {}
+        }
+      }],
     })
     .compileComponents();
   });
--- ngtest/src/app/cmp201231/cmp201231.component.spec.ts
+++ ngtest/src/app/cmp201231/cmp201231.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { Cmp201231Component } from './cmp201231.component';
 
@@ -8,7 +9,8 @@ describe('Cmp201231Component', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ Cmp201231Component ]
+      declarations: [ Cmp201231Component ],
+      imports: [ FormsModule ],
     })
     .compileComponents();
   });
