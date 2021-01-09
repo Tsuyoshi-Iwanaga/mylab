87f571bb16640982cf88b70e395f5a0bd1094cdf
tsuyoshi <tsuyo.1991@gmail.com>
2021-01-09 18:05:23 +0900

    update test sample

--- ngtest/src/app/cmp210107/x-testsample1.spec.ts
+++ ngtest/src/app/cmp210107/x-testsample1.spec.ts
@@ -1,29 +1,26 @@
-import { Cmp201226BookService } from "../cmp201226/cmp201226.book.service";
-import { TruncatePipe } from "./truncate.pipe"
+import { Cmp210107Component } from './cmp210107.component'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
+import { DebugElement } from '@angular/core'
 
-describe('truncate Pipe', () => {
-  let pipe = new TruncatePipe();
-  let msg = 'samplesamplesample';
-
-  it('default', () => {
-    expect(pipe.transform(msg)).toEqual('sample');
-  })
-
-  it('add parameter', () => {
-    expect(pipe.transform(msg)).toEqual('samplesample〜');
-  })
-})
-
-describe('BookService', () => {
-  let service: Cmp201226BookService;
+describe('AppComponent', () => {
+  let debugElement: DebugElement
+  let component: Cmp210107Component
+  let fixture: ComponentFixture<Cmp210107Component>
 
   beforeEach(() => {
-    service = new Cmp201226BookService();
+    TestBed.configureTestingModule({
+      declarations: [Cmp210107Component]
+    })
+
+    fixture = TestBed.createComponent(Cmp210107Component)
+    component = fixture.componentInstance;
+    debugElement = fixture.debugElement.query(By.css('h1'))
   })
 
-  it('getBooksMethod', () => {
-    let books = service.getBooks()
-    expect(books.length).toEqual(5)
-    expect(books[0].title).toEqual('sample books')
+  it('<h1>要素の確認', () => {
+    fixture.detectChanges();
+    const h1 = debugElement.nativeElement;
+    expect(h1.innerText).toMatch(/javascript/i)
   })
 })
