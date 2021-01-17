447a9fcb5607c8860525328538737519ee27738a
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-01-18 01:37:44 +0900

    add render HTML

--- functional_js/func_04.js
+++ functional_js/func_04.js
@@ -22,10 +22,6 @@ const invoices = {
   ]
 }
 
-function statement(invoice, plays) {
-  return renderPlainText(createStatementData(invoice, plays))
-}
-
 function createStatementData(invoices, plays) {
   const statementData = {}
   statementData.customer = invoices.customer
@@ -84,24 +80,46 @@ function createStatementData(invoices, plays) {
   }
 }
 
-function renderPlainText(data, plays) {
+function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays))
+}
+
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays))
+}
+
+function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`
 
   for (let perf of data.performances) {
-    //注文の内訳を出力
     result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`
   }
 
   result += `Amount owed is ${usd(data.totalAmount)}\n`
   result += `You earned ${data.totalVolumeCredits} credits\n`
   return result
+}
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat(
-      "en-US",
-      { style: "currency", currency: "USD", minimumFractionDigits: 2 }
-    ).format(aNumber/100)
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`
+  result += "<table>\n"
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>"
+
+  for (let perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>${usd(perf.amount)}</td><td>${perf.audience}</td></tr>\n`
   }
+
+  result += "</table>\n"
+  result += `<p>Amount owed is ${usd(data.totalAmount)}</p>\n`
+  result += `<p>You earned ${data.totalVolumeCredits} credits</p>\n`
+  return result
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat(
+    "en-US",
+    { style: "currency", currency: "USD", minimumFractionDigits: 2 }
+  ).format(aNumber/100)
 }
 
 console.log(statement(invoices, plays))
