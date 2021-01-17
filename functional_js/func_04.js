f53dd65e148b2521531ffd9062d30247722a7ac7
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-01-18 01:05:52 +0900

    move function playFor

--- functional_js/func_04.js
+++ functional_js/func_04.js
@@ -5,29 +5,46 @@ const plays = {
 }
 
 const invoices = {
-    "customer": "BigCo",
-    "performances": [
-      {
-        playID: "hamlet",
-        audience: 55,
-      },
-      {
-        playID: "as-like",
-        audience: 35,
-      },
-      {
-        playID: "othello",
-        audience: 40,
-      }
-    ]
-  }
+  "customer": "BigCo",
+  "performances": [
+    {
+      playID: "hamlet",
+      audience: 55,
+    },
+    {
+      playID: "as-like",
+      audience: 35,
+    },
+    {
+      playID: "othello",
+      audience: 40,
+    }
+  ]
+}
 
 function statement(invoices, plays) {
-  let result = `Statement for ${invoices.customer}\n`
+  const statementData = {}
+  statementData.customer = invoices.customer
+  statementData.performances = invoices.performances.map(enrichPerformance)
+  return renderPlainText(statementData, plays)
 
-  for (let perf of invoices.performances) {
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance)
+    result.play = playFor(aPerformance)
+    return result
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID]
+  }
+}
+
+function renderPlainText(data, plays) {
+  let result = `Statement for ${data.customer}\n`
+
+  for (let perf of data.performances) {
     //注文の内訳を出力
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
+    result += `${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
   }
 
   result += `Amount owed is ${usd(totalAmount())}\n`
@@ -36,7 +53,7 @@ function statement(invoices, plays) {
 
   function totalAmount() {
     let result = 0
-    for (let perf of invoices.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf)
     }
     return result
@@ -44,7 +61,7 @@ function statement(invoices, plays) {
 
   function totalVolumeCredits() {
     let result = 0
-    for (let perf of invoices.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf)
     }
     return result
@@ -60,17 +77,13 @@ function statement(invoices, plays) {
   function volumeCreditsFor(aPerformance) {
     let result = 0
     result += Math.max(aPerformance.audience - 30, 0)
-    if (playFor(aPerformance).type === "comedy") result += Math.floor(aPerformance.audience / 5)
+    if (aPerformance.play.type === "comedy") result += Math.floor(aPerformance.audience / 5)
     return result
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID]
   }
 
   function amountFor(aPerformance) {
     let result = 0
-    switch(playFor(aPerformance).type) {
+    switch(aPerformance.play.type) {
       case "tragedy":
         result = 40000
         if(aPerformance.audience > 30) {
@@ -85,7 +98,7 @@ function statement(invoices, plays) {
         result += 300 * aPerformance.audience
         break
       default:
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`)
+        throw new Error(`unknown type: ${aPerformance.play.type}`)
     }
     return result
   }
