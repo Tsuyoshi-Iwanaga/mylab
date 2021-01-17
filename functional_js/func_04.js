187c2b3aee4d8e69f8143d13d0dba1e0b1d4bded
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-01-17 23:50:23 +0900

    constract methods & methods inline

--- functional_js/func_04.js
+++ functional_js/func_04.js
@@ -22,45 +22,50 @@ const invoices = {
     ]
   }
 
-function statement (invoices, plays) {
+function statement(invoices, plays) {
   let totalAmount = 0
   let volumeCredits = 0
   let result = `Statement for ${invoices.customer}\n`
-  const format = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2}).format
+  const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format
 
-  for(let perf of invoices.performances) {
-    const play = plays[perf.playID]
-    let thisAmount = 0
+  for (let perf of invoices.performances) {
+    //ボリューム特典のポイントを加算
+    volumeCredits += Math.max(perf.audience - 30, 0)
+    //comedyの時は10人につき、さらにポイントを加算
+    if (playFor(perf).type === "comedy") volumeCredits += Math.floor(perf.audience / 5)
+    //注文の内訳を出力
+    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`
+    totalAmount += amountFor(perf)
+  }
+  result += `Amount owed is ${format(totalAmount / 100)}\n`
+  result += `You earned ${volumeCredits} credits\n`
+  return result
+}
 
-    switch(play.type) {
-      case "tragedy":
-        thisAmount = 40000
-        if(perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30)
-        }
-        break
-      case "comedy":
-        thisAmount = 30000
-        if(perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20)
-        }
-        thisAmount += 300 * perf.audience
-        break
-      default:
-        throw new Error(`unknown type: ${play.type}`)
+function amountFor(aPerformance) {
+  let result = 0
+  switch(playFor(aPerformance).type) {
+    case "tragedy":
+      result = 40000
+      if(aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30)
       }
+      break
+    case "comedy":
+      result = 30000
+      if(aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20)
+      }
+      result += 300 * aPerformance.audience
+      break
+    default:
+      throw new Error(`unknown type: ${playFor(aPerformance).type}`)
+  }
+  return result
+}
 
-      //ボリューム特典のポイントを加算
-      volumeCredits += Math.max(perf.audience - 30, 0)
-      //comedyの時は10人につき、さらにポイントを加算
-      if(play.type === "comedy") volumeCredits += Math.floor(perf.audience / 5)
-      //注文の内訳を出力
-      result += `${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`
-      totalAmount += thisAmount
-    }
-    result += `Amount owed is ${format(totalAmount/100)}\n`
-    result += `You earned ${volumeCredits} credits\n`
-    return result
+function playFor(aPerformance) {
+  return plays[aPerformance.playID]
 }
 
 console.log(statement(invoices, plays))
