a9a4dd2667a27d9f09fdd83a92d63bcaea7983d6
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2021-01-26 09:48:50 +0900

    jest Setup and Teardown

---
+++ jest_sample/004/setup.test.js
@@ -0,0 +1,36 @@
+//セットアップとティアダウン
+
+beforeEach(() => {
+  //すべてのテストの実行前に毎回実行される
+})
+
+beforeAll(() => {
+  //1回だけ実行される
+})
+
+test('setUp test', () => {
+  expect(1 + 1).toBe(2)
+})
+
+afterEach(() => {
+  //すべてのテストの実行後に毎回実行される
+})
+
+afterAll(() => {
+  //最後に1回だけ実行される
+})
+
+//describeでテストをまとめることができる
+describe('matching cities to foods', () => {
+  beforeEach(() => {
+    return initializeFoodDatabase()
+  })
+
+  test('test001', () => {
+    expect(2 + 3).toBe(5)
+  })
+
+  test('test002', () => {
+    expect(3 + 3).toBe(6)
+  })
+})
