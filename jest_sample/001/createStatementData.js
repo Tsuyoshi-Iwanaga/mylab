da4feae16fc924069a3aa13db51f8e8cc18a895a
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-01-31 01:51:32 +0900

    refactoring14: implements class

--- jest_sample/001/createStatementData.js
+++ jest_sample/001/createStatementData.js
@@ -1,3 +1,37 @@
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance
+    this.play = aPlay
+  }
+
+  get amount() {
+    let result = 0
+    switch(this.play.type) {
+      case "tragedy":
+        result = 40000
+        if(this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30)
+        }
+        break
+      case "comedy":
+        result = 30000
+        if(this.performance.audience > 20) {
+          result += 300 * this.performance.audience
+        }
+        break
+      default:
+        throw new Error(`unknown type: ${this.play.type}`)
+    }
+    return result
+  }
+
+  get volumeCredits () {
+    let result = 0
+    result += Math.max(this.performance.audience - 30, 0)
+    if(this.play.type === "comedy") result += Math.floor(this.performance.audience / 5)
+    return result
+  }
+}
 
 function createStatementData(invoice, plays) {
   const result = {}
@@ -8,10 +42,11 @@ function createStatementData(invoice, plays) {
   return result
 
   function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance))
     const result = Object.assign({}, aPerformance)
-    result.play = playFor(result)
-    result.amount = amountFor(result)
-    result.volumeCredits = volumeCreditsFor(result)
+    result.play = calculator.play
+    result.amount = calculator.amount
+    result.volumeCredits = calculator.volumeCredits
     return result
   }
 
@@ -23,36 +58,8 @@ function createStatementData(invoice, plays) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
   }
 
-  function volumeCreditsFor(aPerformance) {
-    let result = 0
-    result += Math.max(aPerformance.audience - 30, 0)
-    if(aPerformance.play.type === "comedy") result += Math.floor(aPerformance.audience / 5)
-    return result
-  }
-
   function playFor(aPerformance) {
     return plays[aPerformance.playID] || {name: 'notFound', type: 'none'}
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0
-    switch(aPerformance.play.type) {
-      case "tragedy":
-        result = 40000
-        if(aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30)
-        }
-        break
-      case "comedy":
-        result = 30000
-        if(aPerformance.audience > 20) {
-          result += 300 * aPerformance.audience
-        }
-        break
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`)
-    }
-    return result
   }
 }
 
