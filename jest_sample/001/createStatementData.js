651313ac7afbee6cc84381504148d75f62d82a61
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-01-31 02:28:40 +0900

    refactoring15: polimophysm

--- jest_sample/001/createStatementData.js
+++ jest_sample/001/createStatementData.js
@@ -1,38 +1,3 @@
-class PerformanceCalculator {
-  constructor(aPerformance, aPlay) {
-    this.performance = aPerformance
-    this.play = aPlay
-  }
-
-  get amount() {
-    let result = 0
-    switch(this.play.type) {
-      case "tragedy":
-        result = 40000
-        if(this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30)
-        }
-        break
-      case "comedy":
-        result = 30000
-        if(this.performance.audience > 20) {
-          result += 300 * this.performance.audience
-        }
-        break
-      default:
-        throw new Error(`unknown type: ${this.play.type}`)
-    }
-    return result
-  }
-
-  get volumeCredits () {
-    let result = 0
-    result += Math.max(this.performance.audience - 30, 0)
-    if(this.play.type === "comedy") result += Math.floor(this.performance.audience / 5)
-    return result
-  }
-}
-
 function createStatementData(invoice, plays) {
   const result = {}
   result.customer = invoice.customer
@@ -42,7 +7,7 @@ function createStatementData(invoice, plays) {
   return result
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance))
+    const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance))
     const result = Object.assign({}, aPerformance)
     result.play = calculator.play
     result.amount = calculator.amount
@@ -60,6 +25,53 @@ function createStatementData(invoice, plays) {
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID] || {name: 'notFound', type: 'none'}
+  }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch(aPlay.type) {
+    case "tragedy": return new TragedyCalculator(aPerformance, aPlay)
+    case "comedy": return new ComedyCalculator(aPerformance, aPlay)
+    default: throw new Error(`unknown type: ${aPlay.type}`)
+  }
+}
+
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance
+    this.play = aPlay
+  }
+
+  get amount() {
+    throw new Error('This is Responsivility of SubClass')
+  }
+
+  get volumeCredits () {
+    return Math.max(this.performance.audience - 30, 0)
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000
+    if(this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30)
+    }
+    return result
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000
+    if(this.performance.audience > 20) {
+      result += 300 * this.performance.audience
+    }
+    return result
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5)
   }
 }
 
