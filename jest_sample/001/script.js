e69a0e71bbcc09014581820b0ac72fe975b462fc
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-01-30 21:43:06 +0900

    refactoring01: Extract function

--- jest_sample/001/script.js
+++ jest_sample/001/script.js
@@ -34,22 +34,7 @@ function statement (invoice, plays) {
     const play = plays[perf.playID]
     let thisAmount = 0
 
-    switch(play.type) {
-      case "tragedy":
-        thisAmount = 40000
-        if(perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30)
-        }
-        break
-      case "comedy":
-        thisAmount = 30000
-        if(perf.audience > 20) {
-          thisAmount += 300 * perf.audience
-        }
-        break
-      default:
-        throw new Error(`unknown type: ${play.type}`)
-    }
+    thisAmount = amountFor(perf, play)
 
     volumeCredits += Math.max(perf.audience - 30, 0)
     if(play.type === "comedy") volumeCredits += Math.floor(perf.audience / 5)
@@ -60,4 +45,26 @@ function statement (invoice, plays) {
   result += `You earned ${volumeCredits} credits`
   return result;
 }
+
+function amountFor(perf, play) {
+  let thisAmount = 0
+  switch(play.type) {
+    case "tragedy":
+      thisAmount = 40000
+      if(perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30)
+      }
+      break
+    case "comedy":
+      thisAmount = 30000
+      if(perf.audience > 20) {
+        thisAmount += 300 * perf.audience
+      }
+      break
+    default:
+      throw new Error(`unknown type: ${play.type}`)
+  }
+  return thisAmount
+}
+
 module.exports = statement
