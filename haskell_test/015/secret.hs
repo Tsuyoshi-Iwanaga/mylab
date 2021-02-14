9b69a9772c050faefbf8426aadbb7c4b6c958afc
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-02-14 15:58:51 +0900

    haskell_lesson15

---
+++ haskell_test/015/secret.hs
@@ -0,0 +1,25 @@
+data FourLetterAlphabet = L1 | L2 | L3 | L4 deriving (Show, Enum, Bounded)
+
+rotN :: (Bounded a, Enum a) => Int -> a -> a
+rotN alphabetSize c = toEnum rotation
+  where
+    halfAlphabet = alphabetSize `div` 2
+    offset = fromEnum c + halfAlphabet
+    rotation = offset `mod` alphabetSize
+
+largestCharNumber :: Int
+largestCharNumber = fromEnum (maxBound :: Char)
+
+rotChar :: Char -> Char
+rotChar charToEncrypt = rotN sizeOfAlphabet charToEncrypt
+  where
+    sizeOfAlphabet = 1 + fromEnum (maxBound :: Char)
+
+fourLetterMessage :: [FourLetterAlphabet]
+fourLetterMessage = [L1, L3, L4, L1, L1, L2]
+
+fourLetterEncoder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
+fourLetterEncoder vals = map rot41 vals
+  where
+    alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
+    rot41 = rotN alphaSize
