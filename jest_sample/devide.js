861d7d14c353d9edcdc0e5776594e3ed4755e032
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2022-05-23 23:32:09 +0900

    add Exception test!

--- jest_sample/devide.js
+++ jest_sample/devide.js
@@ -1,4 +1,5 @@
 function devide(a, b) {
+  if(b === 0) throw new Error()
   return a / b
 }
 module.exports = devide
--- jest_sample/devide.test.js
+++ jest_sample/devide.test.js
@@ -3,3 +3,7 @@ const devide = require('./devide')
 test('devide 4 / 2 equal to 2', () => {
   expect(devide(4, 2)).toBe(2)
 })
+
+test('devide by 0 raise Error', () => {
+  expect(() => devide(1, 0)).toThrow()
+})
