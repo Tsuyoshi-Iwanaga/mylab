5cb88b50e61553072ca12fd8961c31f59be36f17
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2022-12-06 13:29:57 +0900

    remove resources and rename

--- cloudformation/221206/lib/221206-stack.ts
+++
@@ -1,9 +0,0 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-
-export class CDKSampleStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
-
-  }
-}
