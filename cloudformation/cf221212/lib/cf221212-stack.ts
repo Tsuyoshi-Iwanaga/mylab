a4bee1e530cacacaaafe27c2fcaf36a41f02b30f
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2022-12-13 00:15:14 +0900

    update cdk code

--- cloudformation/cf221212/lib/cf221212-stack.ts
+++ cloudformation/cf221212/lib/cf221212-stack.ts
@@ -3,6 +3,7 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { HitCounter } from './hitcounter';
 import { Construct } from 'constructs';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class Cf221212Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -22,6 +23,12 @@ export class Cf221212Stack extends Stack {
     // defines an API Gateway REST API resource backed by our "hello" function
     new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler
+    })
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table,
+      sortBy: '-hits'
     })
   }
 }
