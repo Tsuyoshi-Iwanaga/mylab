944553f12af8c039789ef90c342b11401424fca5
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2022-12-24 21:06:59 +0900

    update listup aws resource

--- cloudformation/check_all_resource.sh
+++ cloudformation/check_all_resource.sh
@@ -1,15 +1,18 @@
 # check all resources in aws accout
 # https://qiita.com/shu85t/items/241ffce4de64370aadbd
 
-for region in `aws ec2 describe-regions --query 'Regions[].RegionName' --region us-west-1 --output text`
+# 実行方法
+# sh check_all_resouce.sh [profile-name]
+
+for region in `aws ec2 describe-regions --query 'Regions[].RegionName' --region us-west-1 --output text --profile $1`
 do
   echo "region = ${region}"
-  aws resourcegroupstaggingapi get-resources --region ${region} --query 'ResourceTagMappingList[].ResourceARN';
+  aws resourcegroupstaggingapi get-resources --region ${region} --query 'ResourceTagMappingList[].ResourceARN' --profile $1;
 done
 
 echo "global"
 echo "IAM"
-aws iam list-users --query 'Users[].Arn'
-aws iam list-roles --query 'Roles[].Arn'
+aws iam list-users --query 'Users[].Arn' --profile $1
+aws iam list-roles --query 'Roles[].Arn' --profile $1
 
 echo "finished"
